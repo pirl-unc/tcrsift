0fe6e21099c899f3b7a49cf559d50c0a2e98bb58
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-05-11 22:23:31 -0400

    Bump version to 0.8.3

--- tcrsift/version.py
+++ tcrsift/version.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 
 version_string = f"v{__version__}"
 
