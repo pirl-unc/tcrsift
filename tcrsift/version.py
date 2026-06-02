c41841f6dd5b590f84beeadd7ad61905c26b4fc1
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-06-01 22:58:15 -0400

    Bump version to 2.11.0

--- tcrsift/version.py
+++ tcrsift/version.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.10.0"
+__version__ = "2.11.0"
 
 version_string = f"v{__version__}"
 
